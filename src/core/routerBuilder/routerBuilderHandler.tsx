a6039b16d9d4e9e46495abd5d17d97153c8e344b
Everton Paixão <everton.paixao16@gmail.com>
2020-04-27 19:17:44 -0300

    delete call not used

--- src/core/routerBuilder/routerBuilderHandler.tsx
+++ src/core/routerBuilder/routerBuilderHandler.tsx
@@ -7,12 +7,6 @@ import PrivateRoute from "./privateRouter";
 const routerBuilder: React.FC = (props: routerProps) => {
   const [acessToken, setAcessToken] = useState(false);
 
-  useEffect(() => {
-    localStorage.getItem("acessToken")
-      ? setAcessToken(true)
-      : setAcessToken(false);
-  }, [acessToken]);
-
   const { modules } = props;
 
   return (
